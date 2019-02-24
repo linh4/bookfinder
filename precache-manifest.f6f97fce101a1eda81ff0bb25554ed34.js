33e5dc21d67ce1483e9f7683b4dea6dd56343cb8
linh4 <linh44h@gmail.com>
2019-02-23 21:59:37 -0500

    Updates

---
+++ precache-manifest.f6f97fce101a1eda81ff0bb25554ed34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea93145390ff8b0cfc07",
+    "url": "/bookfinder/static/js/runtime~main.ea931453.js"
+  },
+  {
+    "revision": "808b398506bcf43a87cd",
+    "url": "/bookfinder/static/js/main.808b3985.chunk.js"
+  },
+  {
+    "revision": "66778f58d72af9cc3df6",
+    "url": "/bookfinder/static/js/2.66778f58.chunk.js"
+  },
+  {
+    "revision": "808b398506bcf43a87cd",
+    "url": "/bookfinder/static/css/main.c1db15b2.chunk.css"
+  },
+  {
+    "revision": "750a5f4d648a2c05b06a4cbc9281e8f0",
+    "url": "/bookfinder/index.html"
+  }
+];
