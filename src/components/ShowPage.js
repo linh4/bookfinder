b48a513bc18d52a2b6a3601e56ef62b5eac51dfb
linh4 <linh44h@gmail.com>
2019-02-24 18:02:06 -0500

    added undefind query

--- src/components/ShowPage.js
+++ src/components/ShowPage.js
@@ -10,7 +10,6 @@ const override = css`
 const imgLink = "https://images.unsplash.com/photo-1537495329792-41ae41ad3bf0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
 
 const renderBooks = (books) => {
-  console.log(books)
   return books.map(book => {
     const info = book.volumeInfo
     return (<div key={book.id} className="each-book">
@@ -19,9 +18,9 @@ const renderBooks = (books) => {
 
       <div className="info-box">
         <h2>{info.title}</h2>
-        {info.authors ? info.authors.map(author => <p key={author}>By: {author}</p>) : <p>By: No authors</p>}
+        {info.authors ? <p className="author">By: {info.authors.join(', ')}</p> : <p>By: No authors information</p>}
 
-        <p>Published By: {info.publisher}</p>
+        {info.publisher ? <p>Published By: {info.publisher}</p> : <p>Published: No Publisher information</p>}
 
         <a className="link-btn" href={info.infoLink} target="_blank" rel="noopener noreferrer">See this Book</a>
       </div>
@@ -31,9 +30,10 @@ const renderBooks = (books) => {
 }
 
 const ShowPage = ({books, loading}) => {
+  console.log(books)
   if (!books) {
     return <>
-      <p className="empty-term">Error: Please provide a search query first!</p>
+      {books === undefined ? <p className="empty-term">No book found! Please enter a different query!</p> : <p className="empty-term">Error: Please provide a search query first!</p> }
       <div className="no-show">Nothing here yet. Try searching for a book</div>
     </>
   }
